911594dc9adbe8ab1b8573ac1a34fb34fb979d42
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-09-17 23:38:18 -0700

    Added animation and changed about style

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+
+jQuery(document).ready(function () {
+    jQuery('.postOne').addClass('hidden').viewportChecker({
+        classToAdd: 'visible animated fadeInLeft',
+        offset: 300
+    });
+});
+
+jQuery(document).ready(function () {
+    jQuery('.postTwo').addClass('hidden').viewportChecker({
+        classToAdd: 'visible animated fadeInRight',
+        offset: 300
+    });
+});	
